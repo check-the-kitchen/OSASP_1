68d12e8d010d09b7c46dcb6516090a280b811a17
check-the-kitchen <83864725+check-the-kitchen@users.noreply.github.com>
2022-03-28 10:30:44 +0300

    Update comp.sh

--- OSASP_lab1/comp.sh
+++ OSASP_lab1/comp.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-#USAGE: ./script.name source_file exe_file_name
- 
-SOURCE="$1"
-EXEFILE="$2"
- 
-if gcc -o "$EXEFILE" "$SOURCE"; then
-    ./"$EXEFILE"
-else
-    echo "Compilation finish with error(s)"
-    exit 1
-fi
+
+gcc $1 -o $2 && ./$2
